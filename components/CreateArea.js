fc1b84400438ddc9f8cc31c507f84702eac64229
spachori11 <spachori93@gmail.com>
2023-04-18 22:09:06 +0530

    Done

---
+++ components/CreateArea.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { Fab, Zoom } from "@mui/material";
+
+function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    if (note.title.trim() === "" || note.content.trim() === "") {
+      alert("Please enter both title and content before submitting.");
+    } else {
+      props.onAdd(note);
+      setNote({
+        title: "",
+        content: "",
+      });
+    }
+    event.preventDefault();
+  }
+
+  function expand() {
+    setExpanded(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        {/* <Zoom in={isExpanded}> */}
+        {/* <Fab onClick={submitNote}> */}
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddCircleIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NotesIcon from '@mui/icons-material/Notes';
+
+function Header() {
+  return (
+    <header>
+      <h2>
+      <NotesIcon/>
+       Note Keeper In Next.js
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
